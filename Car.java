818c45ef21d5f9f5bc9470d938f1a3ac09da22f4
i.tseliuk <7704446@mail.ru>
2019-10-03 11:29:55 +0300

    init

---
+++ Car.java
@@ -0,0 +1,23 @@
+package homework1;
+
+public abstract class Car{
+    private int numberOfSeats;
+    private String driverLicenseAcceptableCategory;
+    public String lock;
+
+    public String getDriverLicenseAcceptableCategory() {
+        return driverLicenseAcceptableCategory;
+    }
+
+    public void setDriverLicenseAcceptableCategory(String driverLicenseAcceptableCategory) {
+        this.driverLicenseAcceptableCategory = driverLicenseAcceptableCategory;
+    }
+
+    public String getLock() {
+        return lock;
+    }
+
+    public void setLock(String lock) {
+        this.lock = lock;
+    }
+}
---
+++ Engine.java
@@ -0,0 +1,31 @@
+package homework1;
+
+public abstract class Engine {
+    private int capacity;
+    private String fuelAcceptableQuality;
+    private String name;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getFuelAcceptableQuality() {
+        return fuelAcceptableQuality;
+    }
+
+    public void setFuelAcceptableQuality(String fuelAcceptableQuality) {
+        this.fuelAcceptableQuality = fuelAcceptableQuality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
