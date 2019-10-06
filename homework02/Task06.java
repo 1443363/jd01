cdf5c46c28faabac2e6364fa3e35a3006ed5a933
1443363 <7704446@mail.ru>
2019-10-06 19:19:51 +0300

    *finished homework02

---
+++ homework02/Task06.java
@@ -0,0 +1,23 @@
+package homework02;
+
+    /*
+    Данный метод не скомпилируется. Отобразится ошибка NPE, т.к. переменные непроинициализированны.
+    И если примитивы int в таком случае по default будут равняться нулю,
+    то переменная Integer = null из-за чего и отобразится данная ошибка.
+    */
+public class Task06 {
+
+    private Integer k;
+    private int z;
+    private int i;
+
+    public void method() {
+        i = k + z;
+    }
+
+    public static void main(String[] args) {
+        Task06 t = new Task06();
+        t.method();
+        System.out.println(t.i);
+    }
+}
--- homework02/main.java
+++ homework02/main.java
@@ -4,25 +4,26 @@ public class main {
 
     public static void main(String[] args) {
         Task01 task01 = new Task01();
-//        task01.shiftRightIgnoreNextChar(255, 5);
-//        task01.shiftRightIgnoreNextChar(255.77, 5);
-//        task01.shiftRightIgnoreNextChar(-1, 24);
+        task01.shiftRightIgnoreNextChar(255, 5);
+        task01.shiftRightIgnoreNextChar(255.77, 5);
+        task01.shiftRightIgnoreNextChar(-1, 24);
 
         Task02 task02 = new Task02();
-//        task02.ANDBitwiseOperation(7,8);
-//        task02.ORBitwiseOperation(8,13);
-//        task02.XORBitwiseOperation(33,54);
-//        task02.ANDLogicalOperation(true, false);
-//        task02.ORLogicalOperation(true, false);
+        task02.ANDBitwiseOperation(7,8);
+        task02.ORBitwiseOperation(8,13);
+        task02.XORBitwiseOperation(33,54);
+        task02.ANDLogicalOperation(true, false);
+        task02.ORLogicalOperation(true, false);
 
         Task03 task03 = new Task03();
-//        task03.getGreaterNumberByTernary(4, 6);
-//        task03.getGreaterNumberByIf(6, 7);
+        task03.getGreaterNumberByTernary(4, 6);
+        task03.getGreaterNumberByIf(6, 7);
 
         Task04 task04 = new Task04();
-//        task04.exampleInstanceOfOperator01();
-//        task04.exampleInstanceOfOperator02();
+        task04.exampleInstanceOfOperator01();
+        task04.exampleInstanceOfOperator02();
 
-
+        Task05 task05 = new Task05();
+        task05.testOperatorPriority();
     }
 }
