7d4bcbee147930df09167e9f295ea9a3c4ce2c90
i.tseliuk <7704446@mail.ru>
2019-10-08 15:41:24 +0300

    started third homework

---
+++ homework03/Main.java
@@ -0,0 +1,10 @@
+package homework03;
+
+public class Main {
+    public static void main(String[] args) {
+        Task01 task01 = new Task01();
+//        task01.viewMultiplicationTable();
+//        task01.multiplicationOfNumbers(8);
+        task01.multipicationofNumericFromNumber(325);
+    }
+}
---
+++ homework03/Task01.java
@@ -0,0 +1,32 @@
+package homework03;
+
+public class Task01 {
+    public void viewMultiplicationTable() {
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j++) {
+                System.out.println(i + " * " + j + " = " + (i * j));
+            }
+        }
+    }
+
+    public void multiplicationOfNumbers(int num) {
+        int multiplicationResult = 1;
+
+        for (int i = 1; i <= num; i++) {
+            System.out.print(multiplicationResult + " * " + i);
+            multiplicationResult *= i;
+            System.out.println(" = " + multiplicationResult);
+        }
+    }
+
+    public void multipicationofNumericFromNumber(int num) {
+        String numbers = String.valueOf(num);
+        int multiplicationResult = 1;
+
+        for (int i = 0; i < numbers.length(); i++) {
+            System.out.print(multiplicationResult + " * " + numbers.charAt(i));
+            multiplicationResult = multiplicationResult * Character.getNumericValue(numbers.charAt(i));
+            System.out.println(" = " + multiplicationResult);
+        }
+    }
+}
